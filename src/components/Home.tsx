c898b3e7f8f8a4ab1e99539ee70857b635a05be1
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-11-13 01:26:13 +0300

    feat: added productList with get request

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import ProductList from '@/components/ProductList';
+
+const Home = () => {
+  
+  return (
+    <div className='w-full h-screen flex items-center justify-center px-[1rem] sm:px-[5rem] lg:px-[8rem] xl:px-[10rem] 2xl:px-[25rem]'>
+      <ProductList />
+    </div>
+  );
+};
+
+export default Home;
